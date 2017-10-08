0d15c737e7a0d284ef38fc6a344907b0365b5e79
azlinrusli <azlinrusli318@gmail.com>
2017-10-08 12:27:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a2brother/azlinruslilab3/FullImageActivity.java
@@ -0,0 +1,27 @@
+package com.example.a2brother.azlinruslilab3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Created by 2BROTHER on 10/8/2017.
+ */
+
+public class FullImageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.full_image);
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+
+    }
+}
